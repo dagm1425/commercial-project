8d4b3761e67f93ddf81332547e786a9ef1bb9d6f
dagm1425 <dagmawinebeyu3@gmail.com>
2024-02-22 15:55:06 +0300

    Add featured videos slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/header/index.jsx';
 import Hero from './components/hero/index.jsx';
 import Projects from './components/projects/index.jsx';
 import Features from './components/features/index.jsx';
+import Videos from './components/videos/index.jsx';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Projects />
       <Features />
+      <Videos />
     </>
   )
 }
