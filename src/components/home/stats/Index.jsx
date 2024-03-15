0e3c1578db7906bec8f067377c27c0ac167ccded
dagm1425 <dagmawinebeyu3@gmail.com>
2024-03-15 11:10:35 +0300

    Add company stats

--- src/components/home/stats/Index.jsx
+++ src/components/home/stats/Index.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useRef } from "react";
 import "./styles.scss";
 import Odometer from "odometer";
+import { PiBuildingsLight } from "react-icons/pi";
+import { MdConstruction } from "react-icons/md";
+import { GiProfit } from "react-icons/gi";
+import { FaPeopleGroup } from "react-icons/fa6";
 
 export default function Index() {
     const stats = useRef(null);
@@ -8,19 +12,23 @@ export default function Index() {
     const statsData = [
         {
             label: "sqm of portfolio",
-            value: "7644"
+            value: "7644",
+            icon: <PiBuildingsLight />
         },
         {
             label: "sqm in development",
-            value: "4130"
+            value: "4130",
+            icon: <MdConstruction />
         },
         {
             label: "billion birr in portfolio value",
-            value: "2.86"
+            value: "2.86",
+            icon: <GiProfit />
         },
         {
-            label: "permanent and temporary employees",
-            value: "471"
+            label: "total employees",
+            value: "471",
+            icon: <FaPeopleGroup />
         }
     ];
 
@@ -56,6 +64,9 @@ export default function Index() {
             {statsData.map((stat, i) => {
                 return (
                     <div key={i} className="statWrapper">
+                        <div className="statIconWrapper">
+                            {stat.icon}
+                        </div>
                         <h1 ref={(ref) => odometerRefs.current.push(ref)} data-value={stat.value} className="odometer">0</h1>
                         <p>{stat.label}</p>
                     </div>
