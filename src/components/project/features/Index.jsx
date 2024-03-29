8bf68527525ad9146cee1197d67bc464febd8753
dagm1425 <dagmawinebeyu3@gmail.com>
2024-03-29 18:23:43 +0300

    Adjust animation positioning and spacing properties

--- src/components/project/features/Index.jsx
+++ src/components/project/features/Index.jsx
@@ -11,8 +11,8 @@ export default function Index({ features }) {
         gsap.to(".projectFeaturesHeader1", {
             scrollTrigger: {
                 trigger: ".projectFeaturesHeader1",
-                start: "top center",
-                end: "+=430px",
+                start: "top 10%",
+                end: "+=1050px",
                 scrub: 2,
                 toggleActions: "restart none none none",
                 pin: true,
@@ -24,9 +24,9 @@ export default function Index({ features }) {
             scrollTrigger: {
               trigger: ".projectFeatures1",
               markers:true, 
-              start: "top center",
-              end: "+=430px",
-              scrub: 3,
+              start: "top 10%",
+              end: "+=1050px",
+              scrub: true,
               pin: true,
               // snap: 1 / (features.length - 1),
             },
@@ -35,52 +35,74 @@ export default function Index({ features }) {
           tl.addLabel("a"); 
 
           tl.to(".projectFeatures1 .projectFeature1:nth-child(2)", {
-            top: "40%",
-            duration: 3
+            top: "20%",
+            // ease: "power1.out" 
+              // duration: 3
           }, "a")
 
           tl.addLabel("b"); 
           
           tl.to(".projectFeatures1 .projectFeature1:nth-child(2)", {
-            top: "1.5%",
-            duration: 5
+            // top: "-0.725rem",
+            top: "0.375rem",
+            // ease: "power1.out" 
+            // duration: 5
           }, "b")
             .to(".projectFeatures1 .projectFeature1:nth-child(1)", {
               scale: ".65",
               marginTop: "-3.1rem",
-              duration: 5
+              // ease: "power1.out" 
+              // duration: 5
             }, "b")
             .to(".projectFeatures1 .projectFeature1:nth-child(3)", {
-              top: "65%",
-              duration: 5
+              top: "28%",
+              // ease: "power1.out" 
+              // duration: 5
             }, "b")
             .addLabel("c"); 
           
           tl.to(".projectFeatures1 .projectFeature1:nth-child(3)", {
-            top: "5%",
-            duration: 5
+            // top: "0.725rem",
+            top: "1.095rem",
+            // ease: "power1.out" 
+            // duration: 5
           }, "c")
             .to(".projectFeatures1 .projectFeature1:nth-child(2)", {
-              scale: ".75",
-              marginTop: "-1.125rem",
-              duration: 5
+              scale: "0.75",
+              transformOrigin: "top",
+              // ease: "power1.out" 
+              // width: "200px",
+              // top: "0.375rem",
+              // marginTop: "-1.125rem",
+              // duration: 5
             }, "c")
             .to(".projectFeatures1 .projectFeature1:nth-child(4)", {
-              top: "70%",
-              duration: 5
+              top: "28%",
+              // ease: "power1.out" 
+              // duration: 5
             }, "c")
             .addLabel("d"); 
 
             tl.to(".projectFeatures1 .projectFeature1:nth-child(3)", {
               scale: ".85",
-              marginTop: "-0.975rem",
-              duration: 5
+              transformOrigin: "top",
+              // ease: "power1.out" 
+              // marginTop: "-0.975rem",
+              // duration: 5
             }, "d")
+            // .to(".projectFeaturesWrapper2", {
+            //   height: "230vh"
+            // }, "d")
             .to(".projectFeatures1 .projectFeature1:nth-child(4)", {
-              top: "6.825%",
-              // top: "0%",
+              // top: "1.825rem",
+              top: "-0.375rem",
+              paddingTop: "3rem",
+              // ease: "power1.out" 
               // marginTop: "-0.5rem",
-              duration: 5
+              // duration: 5
+            }, "d")
+            .to(".projectFeaturesWrapper2", {
+              height: "195vh"
             }, "d")
           
     }, []);
