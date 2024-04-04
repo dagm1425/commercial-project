15066ffff1ab7816dfd9ddb8b79936c28f3b4786
dagm1425 <dagmawinebeyu3@gmail.com>
2024-04-04 11:58:18 +0300

    Modify text content

--- src/components/about/principles/Index.jsx
+++ src/components/about/principles/Index.jsx
@@ -25,17 +25,21 @@ export default function Index() {
     
     const principles = [
         {
-            title: "EXPERIENCE",
+            title: "experience",
             desc: "Addis Skyline Realty is led by an executive body of industry veterans with a long-proven track record of success. Their combined expertise in development, sales and leasing, finance, asset management, operations, and design support Addis Skyline Realty’s strategy of creating extreme value through unique vision and flawless execution.",
         },
         {
-            title: "EXCELLENCE",
-            desc: "At Addis Skyline Realty, excellence is a guiding principle that permeates every project in Addis Ababa, Ethiopia. We strive for unparalleled quality in real estate development, ensuring meticulous attention to detail. From innovative designs to sustainable practices, our commitment to excellence is evident in every aspect of our work. It's not just a standard; it's our unwavering dedication to delivering exceptional properties that stand the test of time.",
+            title: "excellence",
+            desc: "At Addis Skyline Realty, excellence is a guiding principle that permeates every project in Addis Ababa, Ethiopia. We strive for unparalleled quality in real estate development, ensuring meticulous attention to detail. From innovative designs to sustainable practices, our commitment to excellence is evident in every aspect of our work.",
         },
         {
-            title: "EXECUTION",
-            desc: "Execution is a fundamental principle at Addis Skyline Realty, shaping our approach to real estate development in Addis Ababa, Ethiopia. We pride ourselves on translating vision into reality with precision and efficiency. Through meticulous planning, proactive problem-solving, and seamless implementation, we ensure that each project is executed with the utmost professionalism.",
-                }
+            title: "execution",
+            desc: "Execution is a fundamental principle at Addis Skyline Realty, shaping our approach to real estate development in Addis Ababa, Ethiopia. Through meticulous planning, proactive problem-solving, and seamless implementation, we ensure that each project is executed with the utmost professionalism.",
+        },
+        {
+            title: "entrepreneurship",
+            desc: "Addis Skyline Realty's entrepreneurial spirit fuels its ongoing success in transforming real estate. Their bespoke approach to asset management and development, prioritizing tenant and buyer satisfaction at every turn, has cemented their position as an industry leader.",
+        }
     ];
     
     return (
