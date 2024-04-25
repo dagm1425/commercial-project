a0dc714feac387e9b034e82fc6b230004a6dfd59
dagm1425 <dagmawinebeyu3@gmail.com>
2024-04-25 19:14:44 +0300

    Fix indentation

--- src/components/projects/hero/Index.jsx
+++ src/components/projects/hero/Index.jsx
@@ -6,11 +6,11 @@ export default function Index() {
     return (
     <div className={styles["wrapper"]}>
         <div className={styles["projects-hero"]}>
-                <BreadCrumb links={["home", "projects"]} />
-                <h1>residential projects</h1>
-                <p>Whether you seek a cozy standard apartment or a luxurious retreat, our diverse offerings cater to every desire. Immerse yourself in a vibrant community, designed to foster connection and growth. </p>
-                <Overlay />
-            </div>
+            <BreadCrumb links={["home", "projects"]} />
+            <h1>residential projects</h1>
+            <p>Whether you seek a cozy standard apartment or a luxurious retreat, our diverse offerings cater to every desire. Immerse yourself in a vibrant community, designed to foster connection and growth. </p>
+            <Overlay />
         </div>
+    </div>
     )
 }
