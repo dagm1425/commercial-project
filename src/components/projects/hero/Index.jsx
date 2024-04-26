910ef892d8bddfe40f1864952bc347405e41ec57
dagm1425 <dagmawinebeyu3@gmail.com>
2024-04-26 12:13:35 +0300

    Refactor layout

--- src/components/projects/hero/Index.jsx
+++ src/components/projects/hero/Index.jsx
@@ -4,9 +4,9 @@ import Overlay from "../../common/overlay/Index";
 
 export default function Index() {
     return (
-    <div className={styles["wrapper"]}>
+    <div className={styles["projects-wrapper"]}>
+        <BreadCrumb links={["home", "projects"]} />
         <div className={styles["projects-hero"]}>
-            <BreadCrumb links={["home", "projects"]} />
             <h1>residential projects</h1>
             <p>Whether you seek a cozy standard apartment or a luxurious retreat, our diverse offerings cater to every desire. Immerse yourself in a vibrant community, designed to foster connection and growth. </p>
             <Overlay />
